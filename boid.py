0babf02acd1818ca67a59ca9ce0385e16c2d99af
penjikenji <buitien28@gmail.com>
2021-05-17 09:31:51 -0400

    Finalize

--- boid.py
+++ boid.py
@@ -1,28 +1,31 @@
 from p5 import *
-import numpy
+#import numpy
 import random
 
 class Boid():
     def __init__(self):
         self.position = Vector(random.randrange(0, 1080), random.randrange(0, 720))
-        self.max_force = 5
         self.velocity = Vector.random_2D()
         self.velocity.magnitude = random.uniform(2, 4)
         self.acceleration = Vector(0, 0)
-        self.max_speed = 4
-        
 
+        self.max_force = 5 # Force of which boids will flock together
+        self.max_speed = 4 # Boid max speed
+        
+    # Align boids
     def align(self, boids):
         steer = Vector(0, 0)
         max_distance = 50
         total = 0
 
+        # If a boid is near another, then steer towards it
         for bird in boids:
             dist = distance(self.position, bird.position)
             if (bird != self and dist < max_distance):
                 steer += bird.velocity
                 total += 1
 
+        # Average out boid speed with group to follow
         if total > 0:
             steer /= total
             steer.magnitude = self.max_speed
@@ -31,6 +34,7 @@ class Boid():
             
         return steer
 
+    # Stick to flock
     def cohesion(self, boids):
         steer = Vector(0, 0)
         stick_force = 50
@@ -50,10 +54,13 @@ class Boid():
 
         return steer
 
+    # Separate from other boids
     def separation(self, boids):
         steer = Vector(0, 0)
-        sep_force = 50
+        sep_force = 10
         count = 0
+
+        # Find the vector difference and evade
         for bird in boids:
             dist = distance(self.position, bird.position)
             if (bird != self and dist < sep_force):
@@ -70,15 +77,20 @@ class Boid():
 
         return steer
 
+    # group boids together
     def flock(self, boids):
+        # Apply boid rules
         alignment = self.align(boids)
         cohesion = self.cohesion(boids)
         separation = self.separation(boids)
         self.acceleration += separation
         self.acceleration += alignment
         self.acceleration += cohesion
+
+        # Reset to not add up acceleration too much
         self.acceleration.__mul__(0)
 
+    # Boids can never leave the canvas
     def edges(self):
         if self.position.x > 1080:
             self.position.x = 0
@@ -90,14 +102,15 @@ class Boid():
         elif self.position.y < 0:
             self.position.y = 720
 
-
+    # Update location and limit speed of boids
     def update(self):
         self.position += self.velocity
         self.velocity += self.acceleration
         self.velocity.limit(self.max_speed)
 
+    # Show boids using circles
     def show(self):
-        stroke(255)
+        fill(255)
         circle((self.position.x, self.position.y), 10)
 
 
--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 from p5 import *
 from boid import *
-import numpy
-from vispy.util import event
+#import numpy
 
-flock = [Boid() for x in range(10)]
+flock = [Boid() for x in range(50)] # setup boids 
 
+# setup p5 canvas
 def setup():
     size(1080, 720)
 
+# draw onto canvas
 def draw():
-    #this happens every time
     background(30, 30, 47)
 
+    # If the mouse is pressed, then add a boid
     if mouse_is_pressed:
         flock.append(Boid())
+    
+    # If key is pressed, then remove a boid
+    if key_is_pressed:
+        flock.pop(len(flock) - 1)
 
+    # For each boid that's drawn, make it flock and update onto canvas
     for boid in flock:
         boid.edges()
         boid.flock(flock)
         boid.show()
         boid.update()
 
+# Running p5
 if __name__ == "__main__":
     run()
